c4c8bbaab844c024d77ff09cb17507efdeda27eb
Adam O'Callaghan <adam.ocallaghan@yahoo.ie>
2025-02-09 14:03:00 +0000

    page order and some colours changed

--- src/types/pages.ts
+++ src/types/pages.ts
@@ -18,39 +18,39 @@ export const PAGES: PageConfig[] = [
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Dashboard ${i + 1}`, index: i }))
   },
   {
-    name: 'Launch',
+    name: 'Swap',
     color: '#96DCED',
     textColor: '#3D3D3D',
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Launch ${i + 1}`, index: i }))
   },
   {
-    name: 'Swap',
+    name: 'Predict',
     color: '#4D1A28',
     textColor: '#EBADCB',
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Swap ${i + 1}`, index: i }))
   },
   {
     name: 'Lend',
-    color: '#cad9e5',
-    textColor: '#222222',
+    color: '#353f54', //
+    textColor: '#0AEB9A', // 
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Lend ${i + 1}`, index: i }))
   },
   {
-    name: 'Borrow',
+    name: 'Mint',
     color: '#143199',
     textColor: '#dee6ff',
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Borrow ${i + 1}`, index: i }))
   },
   {
-    name: 'Mint',
+    name: 'Launch',
     color: '#222222',
     textColor: '#FF4445',
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Mint ${i + 1}`, index: i }))
   },
   {
-    name: 'Earn',
-    color: '#353f54',
-    textColor: '#0AEB9A',
+    name: 'Social',
+    color: '#cad9e5',
+    textColor: '#222222',
     subPages: Array.from({ length: 4 }, (_, i) => ({ name: `Earn ${i + 1}`, index: i }))
   },
 ]; 
